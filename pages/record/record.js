d1388f72e4477c68495e362d3640e96e8c20dd06
scultc <190758398@qq.com>
2018-12-17 21:14:07 +0800

    我的预约完成

--- pages/record/record.js
+++ pages/record/record.js
@@ -7,8 +7,6 @@ Page({
   data: {
     phoneNumber: null
   },
-  array: [],
-
   search: function(e) {
     this.setData({
       phoneNumber: e.detail
@@ -17,46 +15,90 @@ Page({
   },
   submitPhone() {
     var that = this;
-    if (!this.data.phoneNumber) {
+    if (!this.data.phoneNumber.value) {
       wx.showToast({
         title: '请先输入手机号',
         icon: 'loading',
         duration: 1000,
-        mask:true
+        mask: true
       })
     } else {
       wx.request({
         url: 'https://forklp.cn/position/getreuserinfo',
         method: 'POST',
         data: {
-          phone: that.data.phoneNumber,
+          phone: that.data.phoneNumber.value,
         },
         header: {
           'Content-Type': 'application/x-www-form-urlencoded',
         },
         success: function(res) {
+          console.log(that.data.phoneNumber.value);
           console.log(res.data);
-          that.setData({
-
-          })
-        },fail:function(res){
+          if (res.data.morning) {
+            that.setData({
+              morning: "已预约"
+            })
+          } else {
+            that.setData({
+              morning: "未预约"
+            })
+          }
+          if (res.data.afternoon) {
+            that.setData({
+              afternoon: "已预约"
+            })
+          } else {
+            that.setData({
+              afternoon: "未预约"
+            })
+          }
+          if (res.data.evening) {
+            that.setData({
+              evening: "已预约"
+            })
+          } else {
+            that.setData({
+              evening: "未预约"
+            })
+          }
+          if (res.data.positionId) {
+            that.setData({
+              id: res.data.positionId
+            })
+          } else {
+            that.setData({
+              id: "无"
+            })}
+            if (res.data.positionName) {
+              that.setData({
+                id: res.data.positionName
+              })
+            } else {
+              that.setData({
+                name:"无"
+              })
+              }
+        },
+        fail: function(res) {
           wx.showToast({
             title: '找不到啊',
             icon: 'success',
             duration: 1000,
             mask: true,
-   
+
           })
         }
       })
     }
   },
   delete: function() {
+    var that = this;
     wx.request({
       url: 'https://forklp.cn/position/deletere',
       method: 'POST',
       data: {
-        phone: that.data.phoneNumber
+        phone: that.data.phoneNumber.value
       },
       header: {
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -72,10 +114,10 @@ Page({
   },
 
 
-/**
- * 生命周期函数--监听页面加载
- */
-onLoad: function(options) {
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
 
   },
 
